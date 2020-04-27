c7352afe7a9e5ffbb869debd42ce3540246ee497
root <root@ip-10-0-0-9.eu-west-1.compute.internal>
2020-04-27 15:41:20 +0000

    Source File modified for CICD

--- myproject/src/main/java/com/amit/App.java
+++ myproject/src/main/java/com/amit/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World This is a CICD Pipeline!" );
+        System.out.println( "Hello World This is a demo CICD Pipeline!" );
     }
 }
